bbf83d080f15ca2104769248e8def63fc596019a
Harshithhk <dota2kelkar@gmail.com>
2025-10-04 13:39:20 -0400

    setup a bunch of things - hopefully deploys

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "domain_name" {
+  type = string
+  description = "harshithkelkar.com"
+}
+variable "bucket_name" {
+  type = string
+  description = "harshithkelkar.com"
+}
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
+variable "profile" {
+    type = string
+    default = "default"
+}
