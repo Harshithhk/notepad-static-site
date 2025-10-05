4081e66040821bfd003d67b7ba0c9bb552034d91
Harshithhk <dota2kelkar@gmail.com>
2025-10-04 23:44:39 -0400

    fixed failiure on validate

--- acm.tf
+++ acm.tf
@@ -1,54 +1,35 @@
-# Creates the CloudFront distribution
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name              = aws_s3_bucket.bucket-1.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-    origin_id                = "S3-${var.root_domain_name}"
-  }
+# Requests the SSL certificate for your custom domain
+resource "aws_acm_certificate" "default" {
+  provider          = aws.us-east-1 # CloudFront requires certs in us-east-1
+  domain_name       = "${var.subdomain}.${var.root_domain_name}"
+  validation_method = "DNS"
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
-  aliases = ["${var.subdomain}.${var.root_domain_name}"]
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-${var.root_domain_name}"
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  price_class = "PriceClass_100"
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate_validation.default.certificate_arn
-    ssl_support_method  = "sni-only"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-# Creates the Origin Access Control for S3
-resource "aws_cloudfront_origin_access_control" "default" {
-  name                              = "OAC for ${var.root_domain_name}"
-  description                       = "Origin Access Control for S3 bucket"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
+# Creates the DNS records in Route 53 to validate certificate ownership
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.default.zone_id
+}
+
+# Waits for the certificate to be fully validated before proceeding
+resource "aws_acm_certificate_validation" "default" {
+  provider                = aws.us-east-1
+  certificate_arn         = aws_acm_certificate.default.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
